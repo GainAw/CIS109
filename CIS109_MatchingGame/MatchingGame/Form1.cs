ac4558ff859d25f56e3d90b9affbaa741d479d55
GainAw <wescrank2000@gmail.com>
2019-09-18 20:27:47 -0700

    Project1 - Create Matching Symbols

--- CIS109_MatchingGame/MatchingGame/Form1.cs
+++ CIS109_MatchingGame/MatchingGame/Form1.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/* Wesley Crank
+Reno Techknology Academy
+CIS109: Coding in c3 for imbedded Systems
+Udemy: 17 Begining c# walkthrough Projects Step by Step
+Project 1: Create a Matching pairs icon image Game
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +20,12 @@ namespace MatchingGame
 {
     public partial class Form_Matching_Game : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", ".", ".", "T", "T", "W", "W",
+            "k", "k", "|", "|", "@", "@", "%", "%"
+        };
         public Form_Matching_Game()
         {
             InitializeComponent();
