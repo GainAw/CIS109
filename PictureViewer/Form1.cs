9bf071d375199187ab04e4bc1bd1060bdd59f631
GainAw <wescrank2000@gmail.com>
2019-10-23 15:54:49 -0700

    project 5 - commenting your code

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -19,11 +19,14 @@ namespace PictureViewer
 
         private void Close_Button_Click(object sender, EventArgs e)
         {
+            //closes aplication
             this.Close();
         }
 
         private void SetBGColor_Button_Click(object sender, EventArgs e)
         {
+            //open color dialog
+            //sets background color to selected color
             if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.BackColor = colorDialog1.Color;
@@ -32,11 +35,14 @@ namespace PictureViewer
 
         private void ClearPicture_Button_Click(object sender, EventArgs e)
         {
+            //clears the picture
             pictureBox1.Image = null;
         }
 
         private void ShowPicture_Button_Click(object sender, EventArgs e)
         {
+            //shows the open file dialog
+            //then load selected photo
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 pictureBox1.Load(openFileDialog1.FileName);
@@ -45,6 +51,8 @@ namespace PictureViewer
 
         private void Stretched_Bool_CheckedChanged(object sender, EventArgs e)
         {
+            //If the check is checked
+            //stretch or unstrech the image
             if (Stretched_Bool.Checked)
             {
                 pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
