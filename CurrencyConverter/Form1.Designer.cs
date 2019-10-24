6f1271cbb46effea2b888616e0e4ab2f33754f9d
GainAw <wescrank2000@gmail.com>
2019-10-23 19:40:15 -0700

    Project 7 - Adding components to the form

--- CurrencyConverter/Form1.Designer.cs
+++ CurrencyConverter/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ButtonConverter = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
+            // 
+            // ButtonConverter
+            // 
+            this.ButtonConverter.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ButtonConverter.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.ButtonConverter.Location = new System.Drawing.Point(12, 12);
+            this.ButtonConverter.Name = "ButtonConverter";
+            this.ButtonConverter.Size = new System.Drawing.Size(776, 42);
+            this.ButtonConverter.TabIndex = 0;
+            this.ButtonConverter.Text = "Currency Converter";
+            this.ButtonConverter.UseVisualStyleBackColor = false;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 60);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(776, 378);
+            this.webBrowser1.TabIndex = 1;
             // 
             // CurrencyConverter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.ButtonConverter);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -46,6 +69,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonConverter;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
