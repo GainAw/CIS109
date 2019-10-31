5315bb911ac386a477e5fcd5fe775b7d7c9482a1
GainAw <wescrank2000@gmail.com>
2019-10-30 19:10:14 -0700

    Project 8 - Designing the interface part 2

--- Mozzarella Sticks/Form1.Designer.cs
+++ Mozzarella Sticks/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mozzarella_Sticks
 {
-    partial class Form1
+    partial class Main_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Back_Button = new System.Windows.Forms.ToolStripButton();
+            this.Forward_Button = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Search_Bar = new System.Windows.Forms.ToolStripComboBox();
+            this.Go_Button = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Back_Button,
+            this.Forward_Button,
+            this.toolStripSeparator1,
+            this.Search_Bar,
+            this.Go_Button});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(800, 25);
@@ -48,16 +61,55 @@
             this.webBrowser1.Size = new System.Drawing.Size(800, 425);
             this.webBrowser1.TabIndex = 0;
             // 
-            // Form1
+            // Back_Button
+            // 
+            this.Back_Button.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Back_Button.Image = ((System.Drawing.Image)(resources.GetObject("Back_Button.Image")));
+            this.Back_Button.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Back_Button.Name = "Back_Button";
+            this.Back_Button.Size = new System.Drawing.Size(36, 22);
+            this.Back_Button.Text = "Back";
+            // 
+            // Forward_Button
+            // 
+            this.Forward_Button.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Forward_Button.Image = ((System.Drawing.Image)(resources.GetObject("Forward_Button.Image")));
+            this.Forward_Button.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Forward_Button.Name = "Forward_Button";
+            this.Forward_Button.Size = new System.Drawing.Size(54, 22);
+            this.Forward_Button.Text = "Forward";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // Search_Bar
+            // 
+            this.Search_Bar.Name = "Search_Bar";
+            this.Search_Bar.Size = new System.Drawing.Size(121, 25);
+            // 
+            // Go_Button
+            // 
+            this.Go_Button.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.Go_Button.Image = ((System.Drawing.Image)(resources.GetObject("Go_Button.Image")));
+            this.Go_Button.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Go_Button.Name = "Go_Button";
+            this.Go_Button.Size = new System.Drawing.Size(26, 22);
+            this.Go_Button.Text = "Go";
+            // 
+            // Main_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.toolStrip1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Main_Form";
+            this.Text = "Mozzarella Sticks";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -67,6 +119,11 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripButton Back_Button;
+        private System.Windows.Forms.ToolStripButton Forward_Button;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripComboBox Search_Bar;
+        private System.Windows.Forms.ToolStripButton Go_Button;
     }
 }
 
--- Mozzarella Sticks/Program.cs
+++ Mozzarella Sticks/Program.cs
@@ -16,7 +16,7 @@ namespace Mozzarella_Sticks
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Form());
         }
     }
 }
