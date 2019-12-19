0b0acee25833c1eea4ee73acb2002b1043ac0d71
GainAw <wescrank2000@gmail.com>
2019-12-18 20:02:37 -0800

    Added Random chance for weather
    Had to sign up for most common API's and that involved paying or signing up so let fate decide.

--- LCDScreenClock/LCDScreenClock/MainPage.xaml.cs
+++ LCDScreenClock/LCDScreenClock/MainPage.xaml.cs
@@ -22,11 +22,13 @@ namespace LCDScreenClock
 {
     public sealed partial class MainPage : Page
     {
-
+		Random WeatherRNG;
 		public MainPage()
-        {
+		{
+			WeatherRNG = new Random();
             this.InitializeComponent();
 			displayI2C lcd = new displayI2C(0x27, "I2C1", 0, 1, 2, 4, 5, 6, 7, 3);
+			string weather = (string)_GetWeather();
 			lcd.init();
 			while (true)
 			{
@@ -35,9 +37,38 @@ namespace LCDScreenClock
 				float Sec = DateTime.Now.Second;
 				lcd.gotoxy(4,0);
 				lcd.prints(Hour + ":" + Min + ":" + Sec);
+				lcd.gotoxy(0,1);
+				lcd.prints("Weather: " + weather);
 				Thread.Sleep(1000);
 				lcd.clrscr();
 			}
+		}
+
+		public Object _GetWeather()
+		{
+			Object Weather = "Sunny";
+			switch (WeatherRNG.Next(1, 7))
+			{
+				case 1:
+					Weather = "Sunny";
+					break;
+				case 2:
+					Weather = "Rain";
+					break;
+				case 3:
+					Weather = "Cloudy";
+					break;
+				case 4:
+					Weather = "Hail";
+					break;
+				case 5:
+					Weather = "Clear";
+					break;
+				case 6:
+					Weather = "Bees?";
+					break;
+			}
+			return Weather;
 		}
     }
 }
